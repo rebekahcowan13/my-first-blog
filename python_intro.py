874ff500dc2a6ffdc3ecf1bfcaae62eebc2e11f2
Rebekah Cowan <rebekahcowan13@hotmail.co.uk>
2017-06-03 15:44:21 +0100

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,11 @@
+def hi(name):
+    if name == 'Rebekah':
+        print('hello Rebekah!')
+    elif name == 'Laura':
+        print('hello Laura!')
+    else:
+        print('who are you?')
+
+hi('Rebekah')
+hi('Laura')
+hi('')
